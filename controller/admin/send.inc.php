856e185083c50f82490f6f0aa38de6045bab6840
ZhenyaGOTL <ovchinnikov.zhenya0@gmail.com>
2022-11-03 17:52:33 +0500

    edit

--- controller/admin/send.inc.php
+++ controller/admin/send.inc.php
@@ -4,18 +4,20 @@ namespace DropShipping\Controller\Admin;
 class Send extends \RS\Controller\Admin\Front{
     function actionDrop()
     { 
-        $id = $this->url->request('id', TYPE_STRING, 0);
-        $order_api = new \Shop\Model\OrderApi;
+        $id = $this->url->request('order_id', TYPE_STRING, 0);
+        $order_api = new \Shop\Model\OrderApi();
         $order = $order_api->getElement();
         $order->load($id);
-        var_dump($order['warehouse']);
         $config = \RS\Config\Loader::byModule('dropshipping');
         $c = curl_init('http://api.ds-platforma.ru/ds_order.php');
         $post = [
             'ApiKey' => $config['token'],
             'TestMode' => 1,
             'ExtOrderID' => $id,
-            'dsFio' => $order,
+            'dsFio' => $order->user_fio,
+            'dsMobPhone' => $order->user_phone,
+            'dsEmail' => $order->user_email,
+            
         ];
         $this->result->setSuccess(true);
         $this->result->setSuccessText(t('Изменения успешно сохранены'));
